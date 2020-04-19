bc3e48f8f9453b39047627368eb0a79e6795ad01
bhaskar kumar <bhaskarsahni121@gmail.com>
2020-04-20 02:16:25 +0530

    getting labfood from backendApi

--- components/labradorFood/LabradorPuppyFood.vue
+++ components/labradorFood/LabradorPuppyFood.vue
@@ -1,38 +1,101 @@
 <template>
   <div class="card-columns">
-    <div class="card" style="width:300px">
-      <img src="img/dog/lab-puppy-food.jpg" alt="card-image" />
-      <div>
-        <h2>Price:500Rs</h2>
-      </div>
-      <div>
-        <h1>RoyalCanin</h1>
-      </div>
-    </div>
-    <div class="card" style="width:300px">
-      <img src="img/dog/lab-puppy-food.jpg" alt="card-image" />
-      <div>
-        <h2>Price:500Rs</h2>
-      </div>
-      <div>
-        <h1>RoyalCanin</h1>
-      </div>
-    </div>
-    <div class="card" style="width:300px">
-      <img src="img/dog/lab-puppy-food.jpg" alt="card-image" />
-      <div>
-        <h2>Price:500Rs</h2>
-      </div>
-      <div>
-        <h1>RoyalCanin</h1>
-      </div>
+    <div class="card" v-for="post in posts" :key="post.id">
+      <ul>
+        <li>
+          <div class="name">
+            <h3>{{ post.ProductName }}</h3>
+          </div>
+          <div>
+            <img :src="`${post.ImageUrl}`" />
+          </div>
+          <div class="price">
+            <h3>
+              Price:
+              {{ post.Price }}
+            </h3>
+          </div>
+          <p>Qty:{{count}}</p>
+          <p class="control">
+            <a class="button is-primary" @click.prevent="decrementCounter">-</a>
+            <a class="button is-danger" @click.prevent="incrementCounter">+</a>
+          </p>
+
+          <!-- <button @click.prevent="decrementCounter">-</button>
+          <button @click.prevent="incrementCounter">+</button>
+
+          <button @click="click">AddTocart</button>-->
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
-</script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      posts: [],
+      count: 0
+    };
+  },
+  created() {
+    axios
+      .get("http://localhost:3800/user/getlabpuppyfood")
+      .then(doc => {
+        const postss = doc.data;
+        this.posts = postss;
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    // axios.get("http://localhost:3800/user/getlabpuppyfood").then(doc => {
+    //   var doci = doc.data;
+    //   var i;
+    //   for (i = 0; i < doci.length; i++) {
+    //     this.ImageUrl = doci[i].ImageUrl;
+    //   }
+    // });
+  },
+  methods: {
+    click() {
+      alert("item added");
+    },
+    incrementCounter: function() {
+      this.count += 1;
+    },
+    decrementCounter: function() {
+      this.count -= 1;
+    }
+  }
 
+  // async fetch() {
+  //   this.posts = await this.$axios.$get("http://localhost:3800/user/list");
+  //   this.ImageUrl = await this.$axios
+  //     .$get("http://localhost:3800/user/list")
+  //     .then(doc => {
+  //       var i;
+  //       for (i = 0; i < doc.length; i++) {
+  //         return doc[i].ImageUrl;
+  //       }
+  //     });
+  // },
+  // fetchOnServer: false
+};
+</script>
 <style>
+.NuxtLogo {
+  animation: 1s appear;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,79 +1,78 @@
-
 export default {
-  mode: 'universal',
+  mode: "universal",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: process.env.npm_package_name || 'cares4pet',
+    title: process.env.npm_package_name || "cares4pet",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || "",
+      },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {rel:'stylesheet' , href:'css/bootstrap.min.css'},
-      {rel:'stylesheet', href:'css/animate.css'},
-      {rel:'stylesheet', href:'css/owl.carousel.min.css'},
-      {rel:'stylesheet', href:'css/themify-icons.css'},
-      {rel:'stylesheet', href:'css/flaticon.css'},
-      {rel:'stylesheet' ,href:'css/magnific-popup.css'},
-      {rel:'stylesheet', href:'css/style.css'}
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "stylesheet", href: "css/bootstrap.min.css" },
+      { rel: "stylesheet", href: "css/animate.css" },
+      { rel: "stylesheet", href: "css/owl.carousel.min.css" },
+      { rel: "stylesheet", href: "css/themify-icons.css" },
+      { rel: "stylesheet", href: "css/flaticon.css" },
+      { rel: "stylesheet", href: "css/magnific-popup.css" },
+      { rel: "stylesheet", href: "css/style.css" },
     ],
-    script:[
-      {rel:'text/javascipt', src:'js/jquery-1.12.1.min.js'},
-      {rel:'text/javascipt', src:'js/popper.min.js'},
-      {rel:'text/javascipt', src:'js/bootstrap.min.js'},
-      {rel:'text/javascipt', src:'js/jquery.counterup.min.js'},
-      {rel:'text/javascipt', src:'js/waypoints.min.js'},
-      {rel:'text/javascipt', src:'js/jquery.magnific-popup.js'},
-      {rel:'text/javascipt', src:'js/owl.carousel.min.js'},
-      {rel:'text/javascipt', src:'js/custom.js'}
-    ]
+    script: [
+      { rel: "text/javascipt", src: "js/jquery-1.12.1.min.js" },
+      { rel: "text/javascipt", src: "js/popper.min.js" },
+      { rel: "text/javascipt", src: "js/bootstrap.min.js" },
+      { rel: "text/javascipt", src: "js/jquery.counterup.min.js" },
+      { rel: "text/javascipt", src: "js/waypoints.min.js" },
+      { rel: "text/javascipt", src: "js/jquery.magnific-popup.js" },
+      { rel: "text/javascipt", src: "js/owl.carousel.min.js" },
+      { rel: "text/javascipt", src: "js/custom.js" },
+    ],
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    
-   ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
   /*
-  ** Nuxt.js dev-modules
-  */
+   ** Nuxt.js dev-modules
+   */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
-    
+    "@nuxtjs/eslint-module",
   ],
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    
-  ],
+   ** Nuxt.js modules
+   */
+  modules: ["@nuxtjs/axios"],
+  axios: {
+    // proxyHeaders: false
+  },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     extend(config, ctx) {
       config.module.rules.push({
         test: /\.(ogg|mp3|wav|mpe?g)$/i,
-        loader: 'babel-eslint',
+        loader: "babel-eslint",
         options: {
-          name: '[path][name].[vue]',
+          name: "[path][name].[vue]",
         },
-    
-      })
-    }
-}
-}
+      });
+    },
+  },
+};
