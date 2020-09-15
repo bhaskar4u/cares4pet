9416d484df8a6818745bd3cdb639e43b7291f69f
bhaskar4u <bhaskarsahni121@gmail.com>
2020-09-15 14:08:26 +0530

    initiated cart

---
+++ components/labradorFood/PuppyLabradorFood.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="card-columns">
+      <ul v-for="post in posts" :key="post.id">
+        <li>
+          <div class="card" style="width:200px;">
+            <img :src="`${post.imageUrl}`" alt="card-image" />
+            <h3>Price:{{post.price}}</h3>
+            <button @click="test()">+Add</button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import Axios from "axios";
+export default {
+  data() {
+    return {
+      posts: []
+    };
+  },
+  mounted() {
+    Axios.get("http://13.234.66.240:8000/user/list-puppy-lab-food").then(
+      resData => {
+        const usePost = resData.data;
+        this.posts = usePost;
+        console.log(usePost);
+      }
+    );
+  },
+  methods: {
+    test() {
+      confirm("test is working");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
